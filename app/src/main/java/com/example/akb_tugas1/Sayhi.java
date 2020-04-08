710f1c94cc2057551f0d9ca931c069bf23766863
umpkintoy <umpkintoy@users.noreply.github.com>
2020-04-09 01:41:19 +0700

    Create Sayhi.java

---
+++ app/src/main/java/com/example/akb_tugas1/Sayhi.java
@@ -0,0 +1,14 @@
+package com.example.akb_tugas1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sayhi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sayhi);
+    }
+}
