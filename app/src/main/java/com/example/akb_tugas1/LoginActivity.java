1c3089c721822c48f3add2183f944a655a24f21a
umpkintoy <umpkintoy@users.noreply.github.com>
2020-04-09 01:41:11 +0700

    Create LoginActivity.java

---
+++ app/src/main/java/com/example/akb_tugas1/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.akb_tugas1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void buttonLogin (View view) {
+        Intent intent = new Intent(this, BiodataActivity.class);
+        startActivity(intent);
+    }
+
+}
